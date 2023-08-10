c04ed51f205fa7f6a30b78eb062fd2d8e1ada214
minaerian <121695244+minaerian@users.noreply.github.com>
2023-08-09 23:14:03 -0700

    Update unifi_ssl_setup.sh

--- unifi_ssl_setup.sh
+++ unifi_ssl_setup.sh
@@ -4,8 +4,8 @@
 sudo apt update
 sudo apt install -y certbot
 
-# 2. Obtain the Certificate
-sudo certbot certonly --standalone -d unifi.itgold.net --email mina.info.tech@gmail.com --agree-tos
+# 2. Renew the Certificate (force renewal)
+sudo certbot certonly --standalone -d unifi.itgold.net --email mina.info.tech@gmail.com --agree-tos --force-renewal
 
 # If the certificate is successfully obtained, proceed to configure UniFi
 if [ $? -eq 0 ]; then
@@ -14,13 +14,13 @@ if [ $? -eq 0 ]; then
 
     # Backup the original keystore for safety
     sudo cp /var/lib/unifi/keystore /var/lib/unifi/keystore.backup
-    
+
     # Convert the Let's Encrypt certificate to a format the UniFi Controller can read
     sudo openssl pkcs12 -export -in /etc/letsencrypt/live/unifi.itgold.net/fullchain.pem -inkey /etc/letsencrypt/live/unifi.itgold.net/privkey.pem -out /tmp/cert.p12 -name unifi -password pass:unifi
-    
+
     # Import the new certificate into the UniFi Controller's keystore
     sudo keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /var/lib/unifi/keystore -srckeystore /tmp/cert.p12 -srcstoretype PKCS12 -srcstorepass unifi -alias unifi
-    
+
     # Restart the UniFi Controller to apply the changes
     sudo service unifi restart
 
@@ -33,10 +33,10 @@ if [ $? -eq 0 ]; then
     sudo openssl pkcs12 -export -in /etc/letsencrypt/live/unifi.itgold.net/fullchain.pem -inkey /etc/letsencrypt/live/unifi.itgold.net/privkey.pem -out /tmp/cert.p12 -name unifi -password pass:unifi
     sudo keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /var/lib/unifi/keystore -srckeystore /tmp/cert.p12 -srcstoretype PKCS12 -srcstorepass unifi -alias unifi
     sudo service unifi restart' > /tmp/renew_unifi_cert.sh
-    
+
     # Make the script executable
     sudo chmod +x /tmp/renew_unifi_cert.sh
-    
+
     # Add the renewal command to cron
     echo "@monthly root certbot renew --post-hook '/tmp/renew_unifi_cert.sh'" | sudo tee -a /etc/crontab
 
